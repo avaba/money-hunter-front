3435b5303cdf1170b18185bb139dbb787dc3cbc9
Syuskov Anton <anton.syuskov@gmail.com>
2020-07-12 17:08:41 +0300

    some preparements

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,6 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "prettier/prettier": "off",
-    "no-var-requires": "off"
+    "@typescript-eslint/no-var-requires": "off"
   }
 };
