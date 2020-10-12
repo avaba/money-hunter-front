f8a6cc8e3afd6b2c9a25c3dbe96f209766391cfd
Gogabok <gogabok123@gmail.com>
2020-10-12 03:48:15 +0300

    merge training modals into master

--- src/components/TrainingModal.vue
+++ src/components/TrainingModal.vue
@@ -3,10 +3,12 @@
     <template v-slot:default>
 
       <form action="" class="modal-form">
-        <div class="modal-form__links">
-          <a href="https://ya.ru" target="_blank" @click="handleTrainingLink('Статья')">На ya.ru</a>
-          <a href="https://google.com" target="_blank" @click="handleTrainingLink('Видео')">На google.com</a>
-        </div>
+        <!-- <div class="modal-form__links"> -->
+          <!-- <a href="https://ya.ru" target="_blank" @click="handleTrainingLink('Статья')">На ya.ru</a>
+          <a href="https://google.com" target="_blank" @click="handleTrainingLink('Видео')">На google.com</a> -->
+          <iframe v-if="isShowGoodsSearch" width="560" height="315" src="https://www.youtube.com/embed/pRK9KHqLcgI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe v-if="isShowGoodsFolowing" width="560" height="315" src="https://www.youtube.com/embed/i90wOf9a95k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <!-- </div> -->
       </form>
 
     </template>
@@ -24,10 +26,29 @@
       handleTrainingLink(type) {
         AmplitudeService.tutorial(String(this.$route.name), type)
       }
+    },
+    computed: {
+      isShowGoodsSearch () {
+        return this.$route.matched[0].path=== '/blackbox'
+      },
+      isShowGoodsFolowing () {
+        return this.$route.matched[0].path=== '/tracking'
+      }
     }
   }
 </script>
 
 <style scoped>
+  .modal-form {
+    margin-left: 10px;
+    margin-right: 10px;
+    overflow-y: auto;
+  }
 
+  .modal-form iframe {
+    
+    margin: 20px 0px;
+    min-height: 315px;
+    max-width: 100%;
+  }
 </style>
