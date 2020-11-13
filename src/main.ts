06eac836647ab45436746e048298dc9d05e25409
Gogabok <gogabok123@gmail.com>
2020-11-13 18:12:35 +0300

    Revert "new categories selector"
    This reverts commit 5c0e28688e5b2e2d2a96d306c5fb8b33bfe14fb5.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
-
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Vuetify from 'vuetify';
+
 import './extenders';
-
-import 'vuetify/dist/vuetify.min.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
-
-
-Vue.use(Vuetify)
 
 Vue.config.productionTip = false;
 
